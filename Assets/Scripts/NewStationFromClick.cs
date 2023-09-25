408692ec79c82e6b38080e540ec85067cdedd543
dfardev <dfardev@gmail.com>
2023-09-25 12:41:36 -0700

    Adding Station Button Images

--- Assets/Scripts/NewStationFromClick.cs
+++ Assets/Scripts/NewStationFromClick.cs
@@ -5,25 +5,41 @@ using UnityEngine;
 public class NewStationFromClick : MonoBehaviour
 {
     public GameObject TurretToSpawn;
+    private int x = 0;
     private bool spawningTurret = false;
     private GameObject newTurret; // this is to keep track of the turret we are spawning from the button click
     private Vector3 mousePos; // to keep track of the mouse position 
     // Start is called before the first frame update
     void Start()
     {
-
+        //Debug.Log("Hi");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (spawningTurret == true)
+        {
+            getMousePosition();
+            followMouse();
+            checkForMouseClick();
+        }
     }
 
     private void getMousePosition()
     {
         mousePos = Input.mousePosition;
         mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    private void checkForMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            getMousePosition();
+            newTurret.transform.localPosition = new Vector3(mousePos.x, mousePos.y, 0);
+            spawningTurret = false;
+        }
     }
     public void SpawnNew()
     {
@@ -37,17 +53,20 @@ public class NewStationFromClick : MonoBehaviour
         Debug.Log("definitely firing 2");
 
         spawningTurret = true;
-        while (spawningTurret == true)
-        {
-            getMousePosition();
-            followMouse();
-        }
-        //}
+        Update();
+        //}Debug.Log("Hi");
+        //Debug.Log("Hi");
     }
 
     private void followMouse()
     {
-        newTurret.transform.position = mousePos;
-        spawningTurret = false;
+        newTurret.transform.localPosition = new Vector3(mousePos.x, mousePos.y, 0);
+        //x++;
+        //if(x > 10000000000)
+        //{
+        //    Debug.Log(x);
+        //    spawningTurret = false;
+        //}
+        
     }
 }
