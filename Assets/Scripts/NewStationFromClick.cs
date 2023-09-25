70743cdd36928ce5b3d23be72dcee7213d37153d
dfardev <dfardev@gmail.com>
2023-09-25 16:42:37 -0700

    Fixed station enable/disable issue

--- Assets/Scripts/NewStationFromClick.cs
+++ Assets/Scripts/NewStationFromClick.cs
@@ -6,14 +6,14 @@ using TMPro;
 public class NewStationFromClick : MonoBehaviour
 {
     // Objects to grab
-    public GameObject StationToSpawn; //direct reference to prefab for station 
+   // public GameObject station; //direct reference to prefab for station 
     public GameObject PlayerCurrency; // direct reference to text box for currency
     //public GameObject PlayerCurrency; // direct reference to text box for currency
     private int stationCost; // will grab a cost reference to the universal script for station properties
 
     // Other
-    private string playerCur;
-    private int playerCurrency;
+    private string playerCur; // to convert player currency to string
+    private int moneyValue;
     private bool validCurrency = false; // will keep track of whether player has enough currency
     private bool spawningStation = false; // keeps track of whether a station is ACTIVELY being spawned (player is dragging it on scene)
     private GameObject newstation; // this is to keep track of the station we are spawning from the button click
@@ -52,22 +52,21 @@ public class NewStationFromClick : MonoBehaviour
             enableScripts(); // reactivate the station's script's once placed
         }
     }
-    public void SpawnNew()
+    public void SpawnNew(GameObject stationType)
     {
-        CheckForValidCurrency();
+        Vector2 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CheckForValidCurrency(stationType);
         if (validCurrency == true) // assure player has enough currency to purchase station
         {
-            Vector2 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            newstation = Instantiate(StationToSpawn, currentPosition, Quaternion.identity); //spawn station on the mouse
+            newstation = Instantiate(stationType, currentPosition, Quaternion.identity); //spawn station on the mouse
             spawningStation = true;
-            enableScripts(); // deactivate the station's script's once button is pressed
+            disableScripts(); // deactivate the station's script's once button is pressed
             Update(); // kick off update to track object alongside the cursor
         }
         else
         {
             ///////////////// TODO need to write in some kind of message here for invalid currency
-        }
-
+        } 
     }
 
     void disableScripts()
@@ -93,22 +92,22 @@ public class NewStationFromClick : MonoBehaviour
         newstation.transform.localPosition = new Vector3(mousePos.x, mousePos.y, 0); // keep following the mouse cursor       
     }
 
-    private void CheckForValidCurrency()
+    private void CheckForValidCurrency(GameObject station)
     {
         Debug.Log(PlayerCurrency.GetComponent<TMP_Text>().text);
         playerCur = PlayerCurrency.GetComponent<TMP_Text>().text;
 
         // Debug.Log(PlayerCurrency.GetComponent<Text>().text);
-        int.TryParse(playerCur, out playerCurrency); // grab cost string from text box and parse to int
+        int.TryParse(playerCur, out moneyValue); // grab cost string from text box and parse to int
         //playerCurrency = int.Parse(PlayerCurrency.text);
-        Debug.Log("player currency: " + playerCurrency);
+        Debug.Log("player currency: " + moneyValue);
 
 
-        stationCost = StationToSpawn.GetComponent<StationUniversalProperties>().cost; //get the cost of the station
+        stationCost = station.GetComponent<StationUniversalProperties>().cost; //get the cost of the station
         Debug.Log("station cost: " + stationCost);
-        if (playerCurrency > stationCost) // check if player has enough currency
+        if (moneyValue > stationCost) // check if player has enough currency
         {
-            playerCurrency = playerCurrency - stationCost; // subtract from player currency
+            moneyValue = moneyValue - stationCost; // subtract from player currency
             validCurrency = true;
             UpdateCurrencyUI();
         }
@@ -120,7 +119,7 @@ public class NewStationFromClick : MonoBehaviour
 
     private void UpdateCurrencyUI()
     {
-        playerCur = playerCurrency.ToString();
+        playerCur = moneyValue.ToString();
         PlayerCurrency.GetComponent<TMP_Text>().text = playerCur; // throw new player currency up to UI
     }
 }
